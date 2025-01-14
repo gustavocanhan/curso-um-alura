8fcad3183eb751fc3ab08897fc46bdfcc0c206b0
gustavocanhan <gustavocanhan@gmail.com>
2025-01-14 11:28:23 -0300

    adicionei uma verificação no sorteador de numeros

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -3,8 +3,8 @@ function retornaValor(idHTML) {
   return inputHTML;
 }
 
-function limparValorInput(id){
-  let inputHTML = document.getElementById(id).value = ""
+function limparValorInput(id) {
+  let inputHTML = (document.getElementById(id).value = "");
 }
 
 function exibirValorLabel(id, texto) {
@@ -12,19 +12,19 @@ function exibirValorLabel(id, texto) {
   labelHTML.textContent = texto;
 }
 
-function limparLabel(tag, texto){
-  let labelHTML = document.querySelector(tag)
+function limparLabel(tag, texto) {
+  let labelHTML = document.querySelector(tag);
   labelHTML.textContent = texto;
 }
 
-function alterarStatusBotao(id){
-  let buttonHTML = document.getElementById(id)
-  if (buttonHTML.classList.contains("container__botao-desabilitado")){
-    buttonHTML.classList.remove("container__botao-desabilitado")
-    buttonHTML.classList.add("container__botao")
-  } else{
-    buttonHTML.classList.remove("container__botao")
-    buttonHTML.classList.add("container__botao-desabilitado")
+function alterarStatusBotao(id) {
+  let buttonHTML = document.getElementById(id);
+  if (buttonHTML.classList.contains("container__botao-desabilitado")) {
+    buttonHTML.classList.remove("container__botao-desabilitado");
+    buttonHTML.classList.add("container__botao");
+  } else {
+    buttonHTML.classList.remove("container__botao");
+    buttonHTML.classList.add("container__botao-desabilitado");
   }
 }
 
@@ -32,9 +32,12 @@ function gerarNumeroAleatorio(min, max, quantidade) {
   let listaNumerosAleatorios = [];
 
   while (listaNumerosAleatorios.length != quantidade) {
-    let numeroAleatorio = Math.floor(Math.random() * (max - min + 1) + min);
+    let numeroAleatorio = Math.floor(Math.random() * max + 1);
 
-    if (numeroAleatorio >= min && !(listaNumerosAleatorios.includes(numeroAleatorio))) {
+    if (
+      numeroAleatorio >= min &&
+      !listaNumerosAleatorios.includes(numeroAleatorio)
+    ) {
       listaNumerosAleatorios.push(numeroAleatorio);
     }
   }
@@ -47,29 +50,42 @@ function sortear() {
   let valorMinimo = retornaValor("de");
   let valorMaximo = retornaValor("ate");
 
-  let numerosSorteados = [];
-  numerosSorteados = gerarNumeroAleatorio(valorMinimo, valorMaximo, quantidade);
+  if (valorMaximo - valorMinimo < quantidade) {
+    alert(
+      `Não é possivel gerar a quantidade de números sorteados!\nO intervalo de ${valorMinimo} até ${valorMaximo} é menor do que a quantidade de números (${quantidade}) sorteados solicitado.`
+    );
+    limparValorInput("quantidade");
+    limparValorInput("de");
+    limparValorInput("ate");
+  } else {
+    let numerosSorteados = [];
+    numerosSorteados = gerarNumeroAleatorio(
+      valorMinimo,
+      valorMaximo,
+      quantidade
+    );
 
-  console.log(quantidade)
-  console.log(valorMinimo)
-  console.log(valorMaximo)
-  console.log(numerosSorteados)
+    console.log(quantidade);
+    console.log(valorMinimo);
+    console.log(valorMaximo);
+    console.log(numerosSorteados);
 
-  mensagem = `Números sorteados: ${numerosSorteados}`;
+    mensagem = `Números sorteados: ${numerosSorteados}`;
 
-  exibirValorLabel("#resultado label", mensagem);
+    exibirValorLabel("#resultado label", mensagem);
 
-  alterarStatusBotao("btn-reiniciar")
-  alterarStatusBotao("btn-sortear")
+    alterarStatusBotao("btn-reiniciar");
+    alterarStatusBotao("btn-sortear");
+  }
 }
 
-function reiniciar(){
-  alterarStatusBotao("btn-reiniciar")
-  alterarStatusBotao("btn-sortear")
+function reiniciar() {
+  alterarStatusBotao("btn-reiniciar");
+  alterarStatusBotao("btn-sortear");
 
-  limparValorInput("quantidade")
-  limparValorInput("de")
-  limparValorInput("ate")
+  limparValorInput("quantidade");
+  limparValorInput("de");
+  limparValorInput("ate");
 
-  limparLabel("#resultado label", "Números sorteados: nenhum até agora")
+  limparLabel("#resultado label", "Números sorteados: nenhum até agora");
 }
