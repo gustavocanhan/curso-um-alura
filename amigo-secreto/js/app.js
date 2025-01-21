c82a887f5f4d30789593b7b210779a0bbac3a35c
gustavocanhan <gustavocanhan@gmail.com>
2025-01-21 17:50:41 -0300

    fiz todos os projetos do curso

---
+++ amigo-secreto/js/app.js
@@ -0,0 +1,60 @@
+let amigos = [];
+
+function adicionar() {
+  let amigo = document.getElementById("nome-amigo");
+  let lista = document.getElementById("lista-amigos");
+
+  if (amigos.includes(amigo.value)) {
+    alert("Nome já inserido na lista!\nDigite outro nome.");
+  } else if (amigo.value == "" || amigo.value == " ") {
+    alert("Você tentou adicionar um campo vazio!\nDigite novamente");
+  } else {
+    amigos.push(amigo.value);
+    if (lista.textContent == "") {
+      lista.textContent = amigo.value;
+    } else {
+      lista.textContent = lista.textContent + ", " + amigo.value;
+    }
+  }
+
+  amigo.value = "";
+}
+
+function sortear() {
+  if (amigos.length < 2) {
+    alert(
+      `Não é possivel realizar o sorteio com apenas ${amigos.length} amigo!\nAdicione mais amigos!`
+    );
+    return;
+  }
+  embaralha(amigos);
+  let sorteio = document.getElementById("lista-sorteio");
+
+  for (let i = 0; i < amigos.length; i++) {
+    if (i == amigos.length - 1) {
+      sorteio.innerHTML =
+        sorteio.innerHTML + amigos[i] + " --> " + amigos[0] + "<br>";
+    } else {
+      sorteio.innerHTML =
+        sorteio.innerHTML + amigos[i] + " --> " + amigos[i + 1] + "<br>";
+    }
+  }
+}
+
+function embaralha(lista) {
+  for (let indice = lista.length; indice; indice--) {
+    const indiceAleatorio = Math.floor(Math.random() * indice);
+
+    // atribuição via destructuring
+    [lista[indice - 1], lista[indiceAleatorio]] = [
+      lista[indiceAleatorio],
+      lista[indice - 1],
+    ];
+  }
+}
+
+function reiniciar() {
+  amigos = [];
+  document.getElementById("lista-amigos").innerHTML = "";
+  document.getElementById("lista-sorteio").innerHTML = "";
+}
