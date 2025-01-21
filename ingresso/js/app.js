b54471d1ae85992c0a7e66dba86f3e38e4988cda
gustavocanhan <gustavocanhan@gmail.com>
2025-01-21 14:07:05 -0300

    resolvi o problema do ingresso

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -1,35 +1,79 @@
-let pistaTotal = document.getElementById("qtd-pista").textContent;
-let superiorTotal = document.getElementById("qtd-superior").textContent;
-let inferiorTotal = document.getElementById("qtd-inferior").textContent;
-
 function comprar() {
   let inputIngresso = document.getElementById("tipo-ingresso").value;
-  let inputQuantidade = document.getElementById("qtd").value;
-  alert(inputQuantidade)
+  let inputQuantidade = parseInt(document.getElementById("qtd").value);
 
-  let = mensagem = "Quantidade solicitada não esta disponivel!";
-
-  // Quantidade disponivel
   if (inputIngresso == "inferior") {
-    if (inputQuantidade > inferiorTotal) {
-      alert(mensagem);
-    } else {
-      document.getElementById("qtd-inferior").textContent =
-        inferiorTotal - inputQuantidade;
-    }
+    comprarIgresso(inputQuantidade, "qtd-inferior");
   } else if (inputIngresso == "superior") {
-    if (inputQuantidade > superiorTotal) {
-      alert(mensagem);
-    } else {
-      document.getElementById("qtd-superior").textContent =
-        superiorTotal - inputQuantidade;
-    }
+    comprarIgresso(inputQuantidade, "qtd-superior");
   } else {
-    if (inputQuantidade > pistaTotal) {
-      alert(mensagem);
-    } else {
-      document.getElementById("qtd-pista").textContent =
-        pistaTotal - inputQuantidade;
-    }
+    comprarIgresso(inputQuantidade, "qtd-pista");
   }
 }
+
+function comprarIgresso(quantidade, ingresso) {
+  let ingressoTotal = parseInt(document.getElementById(ingresso).textContent);
+  if (quantidade > ingressoTotal) {
+    return alert("Quantidade solicitada não esta disponivel!");
+  } else {
+    ingressoTotal = ingressoTotal - quantidade;
+    return (document.getElementById(ingresso).textContent = ingressoTotal);
+  }
+}
+
+///////
+//////
+/////
+// OUTRA FORMA DE FAZER
+//////
+////
+//
+// function comprar() {
+//   let inputIngresso = document.getElementById("tipo-ingresso").value;
+//   let inputQuantidade = document.getElementById("qtd").value;
+//   let quantidade = parseInt(inputQuantidade);
+
+//   if (inputIngresso == "inferior") {
+//     comprarInferior(quantidade);
+//   } else if (inputIngresso == "superior") {
+//     comprarSuperior(quantidade);
+//   } else {
+//     comprarPista(quantidade);
+//   }
+// }
+
+// function comprarInferior(quantidade) {
+//   let inferiorTotal = document.getElementById("qtd-inferior").textContent;
+//   if (quantidade > inferiorTotal) {
+//     ingressoIndisponivel();
+//   } else {
+//     inferiorTotal = inferiorTotal - quantidade;
+//     return (document.getElementById("qtd-inferior").textContent =
+//       inferiorTotal);
+//   }
+// }
+
+// function comprarSuperior(quantidade) {
+//   let superiorTotal = document.getElementById("qtd-superior").textContent;
+//   if (quantidade > superiorTotal) {
+//     ingressoIndisponivel();
+//   } else {
+//     superiorTotal = superiorTotal - quantidade;
+//     return (document.getElementById("qtd-superior").textContent =
+//       superiorTotal);
+//   }
+// }
+
+// function comprarPista(quantidade) {
+//   let pistaTotal = document.getElementById("qtd-pista").textContent;
+//   if (quantidade > pistaTotal) {
+//     ingressoIndisponivel();
+//   } else {
+//     pistaTotal = pistaTotal - quantidade;
+//     return (document.getElementById("qtd-pista").textContent = pistaTotal);
+//   }
+// }
+
+// function ingressoIndisponivel() {
+//   return alert("Quantidade solicitada não esta disponivel!");
+// }
